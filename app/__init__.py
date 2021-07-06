71e540ab1e2e5107b2ee800ab6b0abe790ab4bd7
Hyeonuk Kang <hyeonuk@Hyeonukui-MacBookAir.local>
2021-07-06 15:48:42 +0900

    init

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from config import config
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    db.init_app(app)
+    migrate.init_app(app, db=db)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+
+    return app
