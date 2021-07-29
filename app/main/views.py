1a3bbc5ff49205ef0f2c95d1ade232973e63fc8f
hyeonukkang <khu@xinapse.ai>
2021-07-29 12:53:02 +0900

    modify main

--- app/main/views.py
+++ app/main/views.py
@@ -34,7 +34,7 @@ def save_image(form_image):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_image.filename)
     image_fn = random_hex + f_ext
-    image_path = os.path.join(current_app.root_path, 'static/images', image_fn)
+    image_path = os.path.join(current_app.root_path, 'static/covers', image_fn)
 
     form_image.save(image_path)
 
